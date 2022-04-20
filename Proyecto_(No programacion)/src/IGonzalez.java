87b09a4877e520c0e5d1f54bc1118c6b90591576
usuario <usuario@192.168.1.217>
2022-04-20 12:47:22 +0200

    Implementación

--- Proyecto_(No programacion)/src/IGonzalez.java
+++ Proyecto_(No programacion)/src/IGonzalez.java
@@ -1,4 +1,25 @@
 
 public class IGonzalez {
-
+	private String nombre;
+	private String apellidos;
+	
+	//--------- GETTER -----------
+	public String getNombre()
+    {
+            return nombre;
+    }
+	public String getApellidos()
+    {
+            return apellidos;
+    }
+	
+	//---------- SETTER ----------
+	public void setNombre(String nombre)
+	{
+		this.nombre=nombre;	
+	}
+	public void setApellido(String apellidos)
+	{
+		this.apellidos=apellidos;	
+	}
 }
