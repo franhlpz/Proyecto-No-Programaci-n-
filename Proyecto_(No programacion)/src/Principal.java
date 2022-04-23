43ab2ee63874133fc92d798d88bdcc2e4eba022f
isabelglezb <isabelrociogonzalezbecerril@iestorredelrey.es>
2022-04-23 10:31:45 +0200

    Editado por Isabel

--- Proyecto_(No programacion)/src/Principal.java
+++ Proyecto_(No programacion)/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esta es la clase principal del proyecto");
+		System.out.println("Clase principal editada por Isabel");
 	}
 
 }
